4928646e947b74d0d8a3722fdea89d8d86de1f5c
Shrinivas H K <spgrhkadaramandalagi@gmail.com>
2024-12-15 17:40:47 +0530

    Normalize line endings

--- src/App.jsx
+++ src/App.jsx
@@ -6,15 +6,17 @@ import  LoginPage  from '../src/components/AuthComponents/Loginform.jsx';
 import  Register  from '../src/components/AuthComponents/Register.jsx';
 import Header from './components/header.jsx';
 function App() {
+  const authentication = true
+
    
   return (<>
-  
   <Routes>
 <Route path='/' element={<Header/>}></Route>
-<Route path='/auth' element={<AuthPage/>} >
+<Route path='/auth' element={<AuthPage variable={authentication}/>} >
   <Route path='Login' element={<LoginPage/>}></Route>
    <Route path='Register' element={<Register/>}></Route>
 </Route >
+
 
   </Routes>
 
--- src/Page/Authpage.jsx
+++ src/Page/Authpage.jsx
@@ -1,10 +1,11 @@
 import React from 'react';
 
 import { Outlet } from 'react-router-dom';
-function AuthPage(){
+function AuthPage({variable}){
+    console.log(variable)
     return (<>
     <div className='w-full h[100vh]'>
-    <Outlet/>
+       {variable?<Outlet/>:null}
     </div>
     
     
